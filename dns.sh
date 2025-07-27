a39d54dfaa87531ac515350a732d61c2efa88cba
Azumi <azumitan67@gmail.com>
2025-07-27 11:47:14 -0700

    Create dns.sh

---
+++ dns.sh
@@ -0,0 +1,13 @@
+apt update -y
+apt install wget -y
+echo -e "${GREEN}Downloading logo ...${RESET}"
+wget -O /etc/logo2.sh https://github.com/Azumi67/UDP2RAW_FEC/raw/main/logo2.sh
+chmod +x /etc/logo2.sh
+if [ -f "dns.py" ]; then
+    echo -e "${YELLOW}Removing existing dns.py ...${RESET}"
+    rm dns.py
+fi
+echo -e "${YELLOW}Downloading dns.py...${RESET}"
+wget https://github.com/Azumi67/DNS_tun/releases/download/V1.0/dns.py
+echo -e "${GREEN}Launching dns.py...${RESET}"
+python3 dns.py
